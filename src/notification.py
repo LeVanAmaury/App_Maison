0632a9b4c6933927efb82fbdd7283fc17751f72d
AmauryLeVan <amaury24120606@gmail.com>
2026-02-03 20:56:07 +0100

    Update: add more people on tasks

--- src/notification.py
+++ src/notification.py
@@ -1,6 +1,6 @@
 import requests
 
-def send_private_notification(message, target_member):
+def send_private_notification(message, targets_members):
     topics = {
         'Amaury': "famille_caudiu_levan_amaury",
         'Corentin': "famille_caudiu_levan_corentin",
@@ -8,20 +8,21 @@ def send_private_notification(message, target_member):
         'Papoune': 'famille_caudiu_levan_papoune',
         'Maman': 'famille_caudiu_levan_maman'
     }
-    topic=topics.get(target_member)
-    if topic:
-        try:
-            requests.post(f"https://ntfy.sh/{topic}",
-                data=message.encode('utf-8'),
-                headers={
-                    'Title':'Nouvelle tache assignee',
-                    'Priority':'high',
-                    'Tags':'clipboard,memo'
-                }
-            )
-            return True
-        except Exception as err:
-            print(f'Erreur ntfy : {err}')
+    for target_member in targets_members:
+        topic=topics.get(target_member)
+        if topic:
+            try:
+                requests.post(f"https://ntfy.sh/{topic}",
+                    data=message.encode('utf-8'),
+                    headers={
+                        'Title':'Nouvelle tache assignee',
+                        'Priority':'high',
+                        'Tags':'clipboard,memo'
+                    }
+                )
+                return True
+            except Exception as err:
+                print(f'Erreur ntfy : {err}')
+                return False
+        else:
             return False
-    else:
-        return False
--- views/tasks.py
+++ views/tasks.py
@@ -8,14 +8,14 @@ st.title("Liste des tâches")
 # --- SECTION AJOUT DE TÂCHE ---
 with st.form("add_task_form", clear_on_submit=True):
     task_text = st.text_input("Quelle est la tâche ?")
-    member = st.selectbox("Pour qui ?", ["Amaury", "Thais" , "Corentin", "Maman", "Papoune"])
+    members = st.multiselect("Pour qui ?", ["Amaury", "Thais" , "Corentin", "Maman", "Papoune"])
     submit = st.form_submit_button("Ajouter la tache")
 
     if submit and task_text:
-        db.add_task(task_text, member, st.session_state["user"])
+        db.add_task(task_text, members, st.session_state["user"])
         notification_msg = f"Nouvelle mission de {st.session_state["user"]} : {task_text}"
-        send_private_notification(notification_msg, member)
-        st.success(f"Tâche ajoutée et norification envoyée à {member} !")
+        send_private_notification(notification_msg, members)
+        st.success(f"Tâche ajoutée et norification envoyée à {members} !")
         st.rerun()
 
 
@@ -30,11 +30,11 @@ else:
         show_only_todo = st.toggle("N'afficher que les tâches à faire", value=False)
     if show_only_todo:
         tasks = [t for t in tasks if t[3] == False]
-    for t_id, t_title, t_member, t_done, t_created_by in tasks:
+    for t_id, t_title, t_members, t_done, t_created_by in tasks:
         col1, col2, col3 = st.columns([0.7, 0.1, 0.2])
         
         label = f"~~{t_title}~~" if t_done else t_title
-        col1.write(f"Tâche de **{t_created_by}** pour **{t_member}** : {label}")
+        col1.write(f"Tâche de **{t_created_by}** pour **{','.join(map(str,t_members))}** : {label}")
         
         status_done_button = "Fait" if t_done else "A faire"
         if col2.button(status_done_button, key=f"toggle_{t_id}"):
